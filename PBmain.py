ba1ea9da02c8786ffb92a86225f78dca3c98b779
VioletRogue12 <130148039+VioletRogue12@users.noreply.github.com>
2023-06-17 19:54:37 +1000

    imma tryin
    trying to get this to print to txt file

--- PBmain.py
+++ PBmain.py
@@ -1,5 +1,8 @@
 import os
 import csv
+import locale
+
+locale.setlocale(locale.LC_ALL, 'en_AU')
 
 budget_path = os.path.join('PyBank','Resources', 'budget_data.csv')
 
@@ -44,13 +47,34 @@ print (Avg_chg_amount)
 
 #find greatest inrease and decrease
 
-max_date = max(date)
-max_date_index = date.index(max_date)
+Sum_Ttl_chg_amount = 0
+init_amount = [0]
+max_inc = 0
+max_inc_date = ""
+min_inc = 0
+min_inc_date = ""
 
-print (max_date)
-print (max_date_index)
+for i in range(1, len(amount)):
+    chg_amnt = amount[i] - init_amnt
+    Sum_Ttl_chg_amnt += chg_amnt
+    init_amnt = amount[i]
+    if chg_amnt > max_inc:
+        max_inc = chg_amnt
+        max_inc_date = date[i]
+    elif chg_amnt < min_inc:
+        min_inc = chg_amnt
+        min_inc_date= date[i]
+    
 
-#print("Financial Analysis")
-#print("___________________")
+with open("PBmain_Print_Result.txt", 'w') as f:
 
+    print ("Financial Analysis", file=f)
+    print("-------------------------------------", file=f)
+    print (f"Total months:  {total_dates}", file=f)
+    print ("Total: ${:.0f}" .format(Total_amount), file=f)
+    print ("Average change:  ${:.2f}" .format(Avg_chg_amount), file=f)
+    print ("Greatest increase in profits:  " + (max_inc_date) + "  (${:.0f})" .format(max_inc), file=f)
+    print ("Greatest decrease in profits:  " + (min_inc_date) + "  (${:.0f})" .format(min_inc), file=f)
+
+#export results to .txt file
 
