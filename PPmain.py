017cd0c28448bca5fc0b5c3341b29532c4fd894f
VioletRogue12 <130148039+VioletRogue12@users.noreply.github.com>
2023-06-18 11:03:06 +1000

    hmmm, maybe I need to create lists first

--- PPmain.py
+++ PPmain.py
@@ -1,27 +1,33 @@
 import os
 import csv
 
-poll_path = os.path.join('PyPoll','Resources', 'election_data.csv')
+#import data file
 
-#create empty lists to store data
+poll_path_csv = os.path.join('PyPoll','Resources', 'election_data.csv')
 
-
-with open(poll_path) as poll_file:
+with open(poll_path_csv) as poll_file:
     poll_reader = csv.reader(poll_file, delimiter = ",")
-    print (poll_reader)
     poll_data_header = next(poll_reader) #store header
-    print(f"Poll Data Header: {poll_data_header}")
-    
-    Ballot_ID = []
-    County = []
-    Candidate = []
-    
-    for poll_row in poll_reader:
-        Ballot_ID.append(poll_data_header [0])
-        County.append(poll_data_header [1])
-        Candidate.append((poll_data_header [2]))
+    #print(f"Poll Data Header: {poll_data_header}")
+
+# #create function to store data and carry out calculations
+
+def print_candidate_profile (profile_data):
+    Ballot_ID = [str(profile_data[0])]
+    County = [str(profile_data[1])]  #data not required?
+    Candidate = [str(profile_data[2])]
+    Total_votes = len(Ballot_ID) 
+    Candidate_votes = len(Candidate)
+    Candidate_ttl_votes = len(Candidate_votes>0)
+    Candidate_vote_pctg = (Candidate_ttl_votes / Total_votes)*100
+
+    print (print_candidate_profile (profile_data))
+    print (print_candidate_profile (Total_votes))
+  
+
+
         
 
 #calculate total votes cast  
-Total_Votes = len(Ballot_ID)
-print (Total_Votes)
+# Total_Votes = len(Ballot_ID)
+# print (Total_Votes)
