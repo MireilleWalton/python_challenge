81c1888898b381155069a44125eb042562102745
VioletRogue12 <130148039+VioletRogue12@users.noreply.github.com>
2023-06-18 12:19:45 +1000

    feel like i'm heading in the wrong direction

--- PPmain.py
+++ PPmain.py
@@ -3,31 +3,46 @@ import csv
 
 #import data file
 
-poll_path_csv = os.path.join('PyPoll','Resources', 'election_data.csv')
+election_path = os.path.join('PyPoll', 'Resources', 'election_data.csv')
 
-with open(poll_path_csv) as poll_file:
-    poll_reader = csv.reader(poll_file, delimiter = ",")
-    poll_data_header = next(poll_reader) #store header
-    #print(f"Poll Data Header: {poll_data_header}")
+
+Ballot_ID = []
+Candidate = []
+
+with open(election_path) as election_file:
+    election_reader = csv.reader(election_file, delimiter = ",")
+    print (election_reader)
+    election_data = next(election_reader) #store header
+    print(f"Header: {election_data}")  #print budget header
+    for data_row in election_reader:
+        Ballot_ID.append(data_row [0])
+        Candidate.append(str(data_row[2]))
+
+
+Ttl_votes = len(Ballot_ID) 
+Candidate_name = Candidate.count
+Candidate_votes = len(Candidate)
+
+print (Candidate)
 
 # #create function to store data and carry out calculations
 
-def print_candidate_profile (profile_data):
-    Ballot_ID = [str(profile_data[0])]
-    County = [str(profile_data[1])]  #data not required?
-    Candidate = [str(profile_data[2])]
-    Total_votes = len(Ballot_ID) 
-    Candidate_votes = len(Candidate)
-    Candidate_ttl_votes = len(Candidate_votes>0)
-    Candidate_vote_pctg = (Candidate_ttl_votes / Total_votes)*100
+# def print_candidate_profile (profile_data):
+#     #count the votes
+#         Unique_votes = set (Ballot_ID)
+#         total_votes = len(Unique_votes)    
+    
+#         Candidate_ttl_votes = len(Candidate_votes>0)
+#         Candidate_vote_pctg = (Candidate_ttl_votes / Total_votes)*100
 
-    print (print_candidate_profile (profile_data))
-    print (print_candidate_profile (Total_votes))
+
+#         print (print_candidate_profile (profile_data))
+#         print (print_candidate_profile (total_votes))
   
 
 
         
 
-#calculate total votes cast  
-# Total_Votes = len(Ballot_ID)
-# print (Total_Votes)
+# #calculate total votes cast  
+# # Total_Votes = len(Ballot_ID)
+# # print (Total_Votes)
