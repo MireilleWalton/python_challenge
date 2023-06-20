13d0e6dd4d58aef4e40d8b62abcd3d38b99bbb65
VioletRogue12 <130148039+VioletRogue12@users.noreply.github.com>
2023-06-20 18:19:16 +1000

    in the meantime - test file a - final will follow
    a separate test file was created while i worked on two separate peices of code.  This one is version prior to final version

--- PPmain.py
+++ PPmain.py
@@ -4,73 +4,60 @@ import locale
 
 locale.setlocale(locale.LC_ALL, 'en_AU') # code ref: elPastor, 12 March 2017 and S.Lott 26 November 2006 
 
-election_path = os.path.join('PyPoll', 'Resources', 'election_data.csv')
+election_path = os.path.join('PyPoll','Resources', 'election_data.csv')
 
-vote = []
-candidates = []
+#   Read in the CSV file, split data at ','
+with open(election_path, 'r') as csvfile:
+    election_data = csv.reader(csvfile, delimiter=',')
+    header = next(election_data)
 
-with open(election_path) as election_file:
-    election_reader = csv.reader(election_file, delimiter = ",")
-    print (election_reader)
-    election_data = next(election_reader) #store election header
-    #print(f"election Header: {election_data}")  #print election header
-    for data_row in election_reader:
-        vote.append(data_row [0])
-        candidates.append(str(data_row[2]))
-    #print (date)
+#   print out profile - loop through rows
+    for data in election_data:
+     
+#   how many votes? 
+        votes = (election_data[0])
+        candidates = (election_data[2])
+    
+#  count candidate votes
+    ttl_votes = 0
+    for a in votes:
+        ttl_votes = ttl_votes +1 
+        
+    #   nmbr_votes = (vote.count)
+    #   votes = set(nmbr_votes) 
 
-#how many votes? 
-vote = set (vote)
-total_votes = len(vote) # code ref Ulhaq M, 2022 & Bastin N 2010, StackOverflow
-print (total_votes)
+#  count candidate votes
+    candidate_votes = 0
+    for b in candidates:
+        candidate_votes = candidate_votes +1 
+    #   candidate_votes = [candidates.count(name) for name in candidates] 
 
-#candidate vote % - i want to store it in a dictionary
-candidate_votes = {}
-for candidate in candidates:
-    if candidate in candidate_votes:
-        candidate_votes[candidate] +=1
-    else:
-        candidate_votes[candidate] = 1
+#   calculate percentage of votes for each candidate 
+    pctg_votes = []
+    for c in candidates:
+        pctg_votes = (candidate_votes/ttl_votes)*100
+    #   percentage_votes = [(candidate_votes/votes)*100 for votes in candidate_votes]
 
-#candidate vote count - i want to store it in a dictionary
-candidate_pctg = {}
-for candidate in candidates:
-    if candidate in candidate_votes:
-        percentage = (candidate_votes[candidate]/total_votes)*100
-        candidate_pctg[candidate] = percentage
+#   find the winner - code ref: Datagy 2021
+    winner = []
+    for c in candidates:
+        if c in candidate_votes:
+            winner = max(candidate_votes.values())
 
-# find the winner
+#   get candidate names and put them in a list
+    candidate = []
+    for x in candidates:
+        if x in candidate_votes:
+            candidate = candidate_votes +1
 
-winner = max(candidate_votes.values())
-
-#Print election analysis
 
 print("Election Results")
 print("------------------------")
-print(f"Total votes:  {total_votes}")
+print(f"Total votes:  {ttl_votes}")
 print("------------------------")
-print(f"{candidate[0]}, {:.2f} .format{candidate_pctg[percentage]} {candidate_votes[candidate_votes]}}")
-print(f"{candidate[0]}, {:.2f} .format{candidate_pctg[percentage]} {candidate_votes[candidate_votes]}}")
-print(f"{candidate[0]}, {:.2f} .format{candidate_pctg[percentage]} {candidate_votes[candidate_votes]}}")
+print(f"{candidate[0]}, {:.2f} .format{pctg_votes} {:.0f} .format{candidate_votes}")
+print(f"{candidate[1]}, {:.2f} .format{cpctg_votes} {:.0f} .format{candidate_votes}")
+print(f"{candidate[2]}, {:.2f} .format{pctg_votes} {:.0f} .format{candidate_votes}")
 print("------------------------")
-print(f"Winner:  {winner}")
+print(f"Winner:  {winner[0]}")
 print("------------------------")
-
-
-# print (f"Total months:  {total_dates}")
-# print ("Total: ${:.0f}" .format(Total_amount))
-# print ("Average change:  ${:.2f}" .format(Avg_chg_amount))
-# print ("Greatest increase in profits:  " + (max_inc_date) + "  (${:.0f})" .format(max_inc))
-# print ("Greatest decrease in profits:  " + (min_inc_date) + "  (${:.0f})" .format(min_inc))
-
-# # Read in the CSV file
-# with open(election_csv, 'r') as csvfile:
-#     csvreader = csv.reader(csvfile, delimiter=',')
-#     header = next(csvreader)
-
-# #   print out profile - loop through rows
-#     for row in csvreader:
-
-#   If the candidate votes are greater than 0, run the 'print_profile()' function
-    #if election_id_to_check == row[0]:
-#             print_profile(row)
