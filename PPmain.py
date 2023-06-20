77569fa3d9fb7329b29b90417aee5d5d6c900a9d
VioletRogue12 <130148039+VioletRogue12@users.noreply.github.com>
2023-06-21 09:30:44 +1000

    surely i'm almost there

--- PPmain.py
+++ PPmain.py
@@ -4,60 +4,41 @@ import locale
 
 locale.setlocale(locale.LC_ALL, 'en_AU') # code ref: elPastor, 12 March 2017 and S.Lott 26 November 2006 
 
-election_path = os.path.join('PyPoll','Resources', 'election_data.csv')
+election_path = os.path.join('PyPoll', 'Resources', 'election_data.csv')
 
-#   Read in the CSV file, split data at ','
-with open(election_path, 'r') as csvfile:
-    election_data = csv.reader(csvfile, delimiter=',')
-    header = next(election_data)
+vote = []               #  create a list to store total votes
+candidates = []         #  create a list to store candidate names
+candidate_votes = {}    #  create a dictionary to store total candidate votes
+ttl_votes = 0           #  create a counter to count votes
+max_vote = 0            #  create a counter to count max votes
+winner = ""             #  create a repository to store name of winner
 
-#   print out profile - loop through rows
-    for data in election_data:
-     
-#   how many votes? 
-        votes = (election_data[0])
-        candidates = (election_data[2])
-    
-#  count candidate votes
-    ttl_votes = 0
-    for a in votes:
-        ttl_votes = ttl_votes +1 
+with open(election_path) as election_file:
+    election_reader = csv.reader(election_file, delimiter = ",")
+    print (election_reader)
+    election_data = next(election_reader)   #   store election header
+                                           
+    for data_row in election_reader:        #   print(f"election Header: {election_data}")  #print election header
+        ttl_votes = ttl_votes +1            #   count candidate votes
+        candidate = data_row[2]             #   create a list above to store candidate names
+        if candidate not in candidates:     #   code Ref: Tutor session with L Hou, 20 June 2023 - ability to include multiple functions in 1 if statement 
+           candidates.append(candidate) 
         
-    #   nmbr_votes = (vote.count)
-    #   votes = set(nmbr_votes) 
+           candidate_votes[candidate] = 0
+        candidate_votes[candidate] = candidate_votes[candidate]+1
 
-#  count candidate votes
-    candidate_votes = 0
-    for b in candidates:
-        candidate_votes = candidate_votes +1 
-    #   candidate_votes = [candidates.count(name) for name in candidates] 
-
-#   calculate percentage of votes for each candidate 
-    pctg_votes = []
-    for c in candidates:
-        pctg_votes = (candidate_votes/ttl_votes)*100
-    #   percentage_votes = [(candidate_votes/votes)*100 for votes in candidate_votes]
-
-#   find the winner - code ref: Datagy 2021
-    winner = []
-    for c in candidates:
-        if c in candidate_votes:
-            winner = max(candidate_votes.values())
-
-#   get candidate names and put them in a list
-    candidate = []
-    for x in candidates:
-        if x in candidate_votes:
-            candidate = candidate_votes +1
-
+    for cdt in candidate_votes:     #   deterime number of each candidate's votes as well as percentage of votes and 
+        vt = candidate_votes[cdt]   #   vote count for each candidate
+        p = (vt/ttl_votes)*100      #   % of votes for each candidate
+        if vt > max_vote:           #   determine the candidate with the max votes 
+            max_vote=vt
+            winner = cdt 
 
 print("Election Results")
-print("------------------------")
-print(f"Total votes:  {ttl_votes}")
-print("------------------------")
-print(f"{candidate[0]}, {:.2f} .format{pctg_votes} {:.0f} .format{candidate_votes}")
-print(f"{candidate[1]}, {:.2f} .format{cpctg_votes} {:.0f} .format{candidate_votes}")
-print(f"{candidate[2]}, {:.2f} .format{pctg_votes} {:.0f} .format{candidate_votes}")
-print("------------------------")
-print(f"Winner:  {winner[0]}")
-print("------------------------")
+print("----------------------------")
+print(f"Total votes:   {ttl_votes}")
+print("----------------------------") 
+print(cdt,": ", '%.3f' % (p),"% (",vt,")") # code ref Eric Matthes, 2023      
+print("----------------------------") 
+print("Winner:",  winner)
+print("----------------------------") 
