c086304497b1e2292da50edd78c17970dee0138c
VioletRogue12 <130148039+VioletRogue12@users.noreply.github.com>
2023-06-21 15:13:27 +1000

    Final submission - PyPoll

--- PPmain.py
+++ PPmain.py
@@ -12,6 +12,8 @@ candidate_votes = {}    #  create a dictionary to store total candidate votes
 ttl_votes = 0           #  create a counter to count votes
 max_vote = 0            #  create a counter to count max votes
 winner = ""             #  create a repository to store name of winner
+vt_profile = {}         #  create a repository to store name of idividual candidate votes
+pctg_profile ={}        #  create a repository to store name of idividual candidate % of votes
 
 with open(election_path) as election_file:
     election_reader = csv.reader(election_file, delimiter = ",")
@@ -22,23 +24,37 @@ with open(election_path) as election_file:
         ttl_votes = ttl_votes +1            #   count candidate votes
         candidate = data_row[2]             #   create a list above to store candidate names
         if candidate not in candidates:     #   code Ref: Tutor session with L Hou, 20 June 2023 - ability to include multiple functions in 1 if statement 
-           candidates.append(candidate) 
-        
-           candidate_votes[candidate] = 0
+            candidates.append(candidate)    #   add candidates to list
+            candidate_votes[candidate] = 0  #   start dictionary key value at 0
+            vt_profile[candidate] = 0       #   start dictionary key value at 0
+            pctg_profile[candidate]= 0      #   start dictionary key value at 0
         candidate_votes[candidate] = candidate_votes[candidate]+1
+
+with open(os.path.join('PyPoll_main_print_results.txt'), 'w') as f:
+    
+    print("Election Results")               
+    print("Election Results", file=f)
+    print("----------------------------", file=f)
+    print("----------------------------")
+    print(f"Total votes:   {ttl_votes}", file=f)
+    print(f"Total votes:   {ttl_votes}")
+    print("----------------------------", file=f) 
+    print("----------------------------") 
 
     for cdt in candidate_votes:     #   deterime number of each candidate's votes as well as percentage of votes and 
         vt = candidate_votes[cdt]   #   vote count for each candidate
-        p = (vt/ttl_votes)*100      #   % of votes for each candidate
+        pctg = (vt/ttl_votes)*100   #   % of votes for each candidate            
         if vt > max_vote:           #   determine the candidate with the max votes 
             max_vote=vt
-            winner = cdt 
-
-print("Election Results")
-print("----------------------------")
-print(f"Total votes:   {ttl_votes}")
-print("----------------------------") 
-print(cdt,": ", '%.3f' % (p),"% (",vt,")") # code ref Eric Matthes, 2023      
-print("----------------------------") 
-print("Winner:",  winner)
-print("----------------------------") 
+            winner = cdt  
+            vt_profile[cdt] = vt
+            pctg_profile[cdt]= '{:.3f}'.format(pctg)           
+        Output =  f"{cdt}: {pctg:.3f}% ({vt})\n"
+        print(Output, end="", file=f) 
+        print(Output, end="")   
+    print("----------------------------", file=f) 
+    print("----------------------------") 
+    print("Winner:",  winner, file=f)
+    print("Winner:",  winner)
+    print("----------------------------", file=f) 
+    print("----------------------------") 
